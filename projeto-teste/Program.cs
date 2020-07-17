4a9a9a05d3d5518041e3a807203bcff7f3bf7bde
Wesley <wesleygp13@hotmail.com>
2020-07-17 16:33:58 -0300

    Foi acrescentada uma mensagem de Bom dia

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -7,6 +7,7 @@ namespace projeto_teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia testando o git");
         }
     }
 }
